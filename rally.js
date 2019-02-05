b1faa2f391261af71567c85bb413f45eabe42f29
mohur <m.ashrahfi@gmail.com>
2019-02-04 18:30:38 -0800

    Mouse positioning for paddles

---
+++ rally.js
@@ -0,0 +1,10 @@
+function calculateMousePos (evt) {
+	var rect = canvas.getBoundingClientRect();
+	var root = document.documentElement;
+	mouseX = evt.clientX - rect.left - root.scrollLeft;
+	mouseY = evt.clientY - rect.top - root.scrollTop;
+	return {
+			x:mouseX,
+			y:mouseY
+	};
+}
