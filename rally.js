5949661b8599ec79951aaee1ae9952aba80ca336
mohur <m.ashrahfi@gmail.com>
2019-02-04 18:35:49 -0800

    Ball bounce parameters

--- rally.js
+++ rally.js
@@ -8,3 +8,60 @@ function calculateMousePos (evt) {
 			y:mouseY
 	};
 }
+
+function computerMovement() {
+	
+	var paddle2YCenter = paddle2Y + (PADDLEHEIGHT/2);
+	if(paddle2YCenter < ballY) {
+		paddle2Y += 6;
+	} else {
+			paddle2Y -= 6;
+		}
+    }
+
+    
+function moveEverything () {
+    computerMovement();
+    
+    ballX = ballX + ballSpeedX;
+    ballY = ballY + ballSpeedY;
+    
+    if (ballX < 40) {
+        if(ballY > paddle1Y && ballY < paddle1Y+PADDLEHEIGHT) {
+            ballSpeedX = -ballSpeedX;
+    
+            var deltaY = ballY
+            -(paddle2Y+PADDLEHEIGHT/2);
+            ballSpeedY = deltaY * 0.25;
+    
+        }
+    
+        else {
+            ballReset();
+            playerTwoScore++;
+            } 
+        }
+    
+    
+    if (ballX > 860) {
+        if(ballY > paddle2Y && ballY < paddle2Y+PADDLEHEIGHT) {
+            ballSpeedX = -ballSpeedX;
+            }
+    
+        else {
+            ballReset();
+    
+            playerOneScore++;
+            } 
+        }
+    
+    if (ballY < 20) {
+        ballSpeedY = -ballSpeedY;
+        }
+    
+    if (ballY > 480) {
+        ballSpeedY = -ballSpeedY;
+        }
+    
+    
+}
