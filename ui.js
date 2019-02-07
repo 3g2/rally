2f1b7914c324ec6f007ea0a9a62d17bc1e8bf584
mohur <m.ashrahfi@gmail.com>
2019-02-06 18:35:09 -0800

    Declared who is winner & drew net for center

--- ui.js
+++ ui.js
@@ -1,13 +1,24 @@
 function drawEverything () {
 
     colorRect(0, 0, canvas.width, canvas.height, 'black');
+
     
     if(showingWinScreen){
         canvasContext.fillStyle = 'white';
-        canvasContext.fillText("Game Over. Click to Continue.", 100, 100);
-        return;
+        canvasContext.fillText("Game Over. Click to Continue.", 450, 150);
+
+        if (playerOneScore >= WINNING_SCORE) {
+            canvasContext.fillText("Player one has won!")
+        }
+        else if (playerTwoScore >= WINNING_SCORE) {
+            canvasContext.fillText("Player two has won!", 450, 100)
+        }
+
+    return;
     }
 
+    drawNet();
+    
 	colorRect(0, paddle1Y, PADDLE_THICKNESS, PADDLE_HEIGHT, 'white');
 	colorRect(canvas.width - PADDLE_THICKNESS, paddle2Y, PADDLE_THICKNESS, PADDLE_HEIGHT, 'white');
 	
@@ -29,4 +40,11 @@ function colorCircle(centerX, centerY, radius, drawColor) {
 function colorRect(leftX, topY, width, height, drawColor) {
 	canvasContext.fillStyle = drawColor;
 	canvasContext.fillRect(leftX, topY, width, height);
+}
+
+function drawNet() {
+    for (i = 0; i < canvas.height; i += 35) {
+        colorRect(canvas.width/2-1,i,2,20,'white');
+    }
+
 }
